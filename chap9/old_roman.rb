0aafc8ed002c7244c51e24c7e531cd5b419886f5
Bill Lindeman <bill.lindeman@icloud.com>
2013-09-26 23:22:56 -0700

    chapter nine submission

---
+++ chap9/old_roman.rb
@@ -0,0 +1,13 @@
+def old_roman_numeral(number)
+  numeral = ''
+  
+  numeral = numeral + 'M' * (number / 1000)
+  numeral = numeral + 'D' * (number % 1000 / 500)
+  numeral = numeral + 'C' * (number % 500 / 100)
+  numeral = numeral + 'L' * (number % 100 / 50)
+  numeral = numeral + 'X' * (number % 50 / 10)
+  numeral = numeral + 'V' * (number % 10 / 5)
+  numeral = numeral + 'I' * (number % 5 / 1)
+  numeral
+end
+puts (old_roman_numeral(1999))
